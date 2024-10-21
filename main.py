1b54ca5e0530701c02813c75381bf1a1c6677ea0
Marvefect <125153184+Marvefect@users.noreply.github.com>
2024-10-21 18:21:47 +0300

    Upd

--- main.py
+++ main.py
@@ -2,32 +2,24 @@ import os
 from os import listdir
 import shutil
 import platform
+import time
+import threading
 
-if platform.system() == "Windows":
-    div = "\\"
-    print("Detected Windows")
-elif platform.system() == "Linux":
-    div = "/"
-    print("Detected Linux")
-else:
-    print("Unsupported OS! (MacOs Support Soon-ish TM)")
-    n = int(input())
-    exit()
+def OS():
+    if platform.system() == "Windows":  return "\\"
+    elif platform.system() == "Linux":  return "/"
+    else: print("OS Unsupported."); n - int(input)
 
-print("Marvefect's Rate Remover \nMade for o!Lazer players")
-
-print("Input Directory to take songs from: \nExample: C:\\Users\\user\\RateRemover\\Input\\")
-dir_in = str(input())
-print("Output Directory to put resulting songs in: \nExample: C:\\Users\\user\\RateRemover\\Output\\")
-dir_out = str(input())
-file_list = [f for f in listdir(dir_in)]
-print("Found", len(file_list), "Files, \nWorking...")
-
-for f in file_list:
+def Purge(dir_in, dir_out, f):
     print(f)
     f_dir = dir_in + f
     f_out = dir_out + f + div
-    shutil.unpack_archive(f_dir, f_out, "zip")
+    if f.endswith(".osz"):
+        shutil.unpack_archive(f_dir, f_out, "zip")
+        os.remove(f_dir)
+    else:
+        shutil.copytree(f_dir, f_out)
+        shutil.rmtree(f_dir + div)
     mp3_files = [m for m in listdir(f_out) if m.endswith(".mp3")]
     m_file = min(mp3_files, key=len)
     for m in mp3_files:
@@ -39,8 +31,20 @@ for f in file_list:
         if result == -1:
             os.remove(f_out + o)
     shutil.make_archive(dir_out + f, "zip", f_out)
-    os.remove(f_dir)
     shutil.rmtree(f_out)
     os.rename(dir_out + f + ".zip", dir_out + f)
-print("Done")
-n = int(input()) 
+
+if __name__ == "__main__":
+    print("Marvefect's Rate Remover \nMade for o!Lazer players")
+    div = OS()
+    print("Input Directory to take songs from: \nExample: C:\\Users\\user\\RateRemover\\Input\\")
+    dir_in = str(input())
+    print("Output Directory to put resulting songs in: \nExample: C:\\Users\\user\\RateRemover\\Output\\")
+    dir_out = str(input())
+    start = time.time()
+    for f in listdir(dir_in):
+        thread = threading.Thread(target=Purge, args=(dir_in, dir_out, f))
+        thread.start()
+    thread.join()
+    end = time.time()
+    print("Done! Time taken: ", (end-start) * 10**3, "ms")
